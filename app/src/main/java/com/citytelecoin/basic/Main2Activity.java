7b98424254b9f03890f771de66ddc285f8fd6689
carlosp <carlosp@citytelecoin.com>
2017-11-13 10:03:14 -0600

    Took away ability to power off device from the app selection screen.  !
    Following code bit was added to top of file MainActivity2.java
    ____________________________________________________________
    
        @Override
        public void onWindowFocusChanged(boolean hasFocus) {
            super.onWindowFocusChanged(hasFocus);
            if (!hasFocus) {
                // Close every kind of system dialog
                Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
                sendBroadcast(closeDialog);
            }
        }

--- app/src/main/java/com/citytelecoin/basic/Main2Activity.java
+++ app/src/main/java/com/citytelecoin/basic/Main2Activity.java
@@ -1,42 +1,21 @@
 package com.citytelecoin.basic;
 
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.List;
 
 public class Main2Activity extends Activity {
 
@@ -52,7 +31,15 @@ public class Main2Activity extends Activity {
 
     }
 
-
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus) {
+            // Close every kind of system dialog
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
 
 
     public class AppDetail {
@@ -60,6 +47,7 @@ public class Main2Activity extends Activity {
         CharSequence name;
         Drawable icon;
     }
+
 
 
 
@@ -71,15 +59,21 @@ public class Main2Activity extends Activity {
         Intent i = new Intent(Intent.ACTION_MAIN, null);
         i.addCategory(Intent.CATEGORY_LAUNCHER);
         List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
-        for(ResolveInfo ri:availableActivities){
+        for(ResolveInfo ri:availableActivities)
+
+
+        {
             AppDetail app = new AppDetail();
             app.label = ri.loadLabel(manager);
             app.name = ri.activityInfo.packageName;
             app.icon = ri.activityInfo.loadIcon(manager);
             apps.add(app);
-        }
-    }
 
+
+        }
+
+
+    }
 
     private ListView list;
     private void loadListView(){
